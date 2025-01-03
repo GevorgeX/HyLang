92ceb5b40f14ecbb5dfb1a59068fb803a29a531e
Gevorg Navoyan <gevnavoyan@gmail.com>
2025-01-03 12:15:12 +0400

    Adding AST Print

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -2,8 +2,30 @@
 #include <iostream>
 #include <sstream>
 
+#include "ast/nodes/binaryExpNode.h"
+#include "ast/nodes/integerExpNode.h"
+#include "ast/nodes/unaryExpNode.h"
 #include "lexer/lexer.h"
 #include "parser/parser.h"
+
+void print_ast( Node& root,std::string ident = "") {
+    #define instanceOf(type, target) auto* exp = dynamic_cast<type*>(&target)
+    std::cout << ident <<"+- ";
+    ident.append("|  ");
+
+    if (instanceOf(IntegerExpNode,root)) {
+        std::cout << "Integer: " << exp->value <<"\n";
+    }
+    else if (instanceOf(BinaryExpNode,root)) {
+        std::cout<<"Binary op: "<<exp->opType<<"\n";
+        print_ast(*exp->left,ident);
+        print_ast(*exp->right,ident);
+    }
+    else if (instanceOf(UnaryExpNode,root)) {
+        std::cout<<"Unary op: "<<exp->opType<<"\n";
+        print_ast(*exp->value,ident);
+    }
+}
 
 int main(int argc, char **argv) {
     std::ifstream file;
@@ -28,4 +50,5 @@ int main(int argc, char **argv) {
     Parser parser(tokens, code);
     auto t = parser.parse();
 
+    print_ast( *t);
 }
